1cdbbb2556dc09138a7d1d94af5e4eb7ffbcfb54
Mike Murray <michaelbrazell@gmail.com>
2018-06-20 17:38:55 -0400

    Add bootstrap, strip out default styling

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/header'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+// import './index.css'
 
 const Layout = ({ children, data }) => (
   <div>
@@ -15,14 +17,7 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div className="container">
       {children()}
     </div>
   </div>
